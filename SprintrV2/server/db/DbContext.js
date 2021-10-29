5ba6a23ceb588d3d25268f2cc00f7c5511699882
KyleMHanna <ikyle.hanna@gmail.com>
2021-10-29 16:25:43 -0600

    Everything added to dbcontext ✔️

--- SprintrV2/server/db/DbContext.js
+++ SprintrV2/server/db/DbContext.js
@@ -1,11 +1,21 @@
 import mongoose from 'mongoose'
 import { AccountSchema, ProfileSchema } from '../models/Account'
+import { BackLogItemSchema } from '../models/BackLogItem.js'
+import { NoteSchema } from '../models/Note.js'
+import { ProjectSchema } from '../models/Project.js'
+import { SprintSchema } from '../models/Sprint.js'
+import { TaskSchema } from '../models/Task.js'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   Profiles = mongoose.model('Profile', ProfileSchema, 'accounts');
+  Project = mongoose.model('Project', ProjectSchema, 'accounts');
+  BackLogItem = mongoose.model('BackLogItem', BackLogItemSchema);
+  Note = mongoose.model('Note', NoteSchema);
+  Sprint = mongoose.model('Sprint', SprintSchema);
+  Task = mongoose.model('Task', TaskSchema);
 }
 
 export const dbContext = new DbContext()
