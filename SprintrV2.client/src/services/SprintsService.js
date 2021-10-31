a2bacf923180a4fa96d492e1350bbbc547f77616
KyleMHanna <ikyle.hanna@gmail.com>
2021-10-31 12:39:13 -0600

    getSprints done

--- SprintrV2.client/src/services/SprintsService.js
+++ SprintrV2.client/src/services/SprintsService.js
@@ -1,6 +1,14 @@
+import { AppState } from "../AppState.js";
+import { Sprint } from "../models/Sprint.js";
+import { logger } from "../utils/Logger.js";
+import { api } from "./AxiosService.js";
 
 class SprintsService {
-
+  async getSprints(projectId) {
+    const res = await api.get(`api/projects/${ projectId }/sprints`)
+    logger.log(res, 'Getting Sprints')
+    AppState.sprints = res.data.map(s => new Sprint(s))
+  }
 }
 
 export const socketService = new SprintsService();
